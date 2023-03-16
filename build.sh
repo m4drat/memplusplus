230a5b86166d53f819bf5c285bdafc2f69cf7a9f
Theodor <ftri2001@gmail.com>
2023-03-16 11:36:43 +0300

    Added CodeQL build for build.sh [skip-bench]

--- build.sh
+++ build.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-CXX=clang++-15 cmake -S . -B build \
+if [ "$1" == "codeql" ]; then
+	echo "Building for CodeQL"
+
+	CXX=clang++ cmake -S . -B build \
+		-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+		-DCMAKE_BUILD_TYPE=Debug \
+		-DMPP_BUILD_FUZZER=ON \
+		-DMPP_BUILD_EXAMPLE=ON \
+		-DMPP_BUILD_TESTS=OFF \
+		-DMPP_BUILD_DOCS=OFF \
+		-DMPP_SANITIZERS=OFF \
+		-DMPP_VALGRIND=OFF \
+		-DMPP_BUILD_SHARED_LIBS=OFF \
+		-DMPP_FULL_DEBUG=ON \
+		-DMPP_SECURE=ON \
+		-DMPP_PROFILE=ON \
+		-DMPP_COLOUR=ON \
+		-DMPP_STATS=ON
+
+	CXX=clang++ cmake --build build --config Debug --target all -- -j 16
+
+	exit 0
+fi
+
+CXX=clang++ cmake -S . -B build \
 	-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
 	-DCMAKE_BUILD_TYPE=Debug \
 	-DMPP_BUILD_FUZZER=ON \
@@ -16,4 +40,4 @@ CXX=clang++-15 cmake -S . -B build \
 	-DMPP_COLOUR=ON \
 	-DMPP_STATS=ON
 
-CXX=clang++-15 cmake --build build --config Debug --target all -- -j 16
+CXX=clang++ cmake --build build --config Debug --target all -- -j 16
